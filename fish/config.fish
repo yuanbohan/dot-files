657eb625ccbb053803795d474572f7e7ce63a1b8
yuanbohan <yuanbo.han@gmail.com>
2020-03-21 23:27:14 +0800

    update fish,terminator after install archlinux

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+# proxy
+set -x no_proxy "localhost, 127.0.0.1, local, ::1,"
+set -x http_proxy "http://127.0.0.1:8118"
+set -x https_proxy "http://127.0.0.1:8118"
+
+# go
+set -x GOPATH $HOME/go-place
+
+# PATH
+set -x PATH $HOME/.cargo/bin /usr/local/go/bin $GOPATH/bin $PATH
+
+# etcd
+set -x ETCDCTL_API 3
+
+# python
+# alias python="python3"
+# alias pip="pip3"
+# export WORKON_HOME=$HOME/.virtualenvs
+# export PROJECT_HOME=$HOME/python-place
+# export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+# source ~/.local/bin/virtualenvwrapper.sh
+
+# fish
+if test -f /home/bobo/.autojump/share/autojump/autojump.fish; . /home/bobo/.autojump/share/autojump/autojump.fish; end

4089910e3dcda2ee3bc35e8e06df798020e7bc16
yuanbohan <yuanbo.han@gmail.com>
2020-03-26 01:43:12 +0800

    powerline for fish and tmux

--- fish/config.fish
+++ fish/config.fish
@@ -4,25 +4,34 @@ set -x http_proxy "http://127.0.0.1:8118"
 set -x https_proxy "http://127.0.0.1:8118"
 
 # go
-set -x GOPATH $HOME/go-place
-
-# PATH
-set -x PATH $HOME/.cargo/bin /usr/local/go/bin $GOPATH/bin $PATH
+set -x GOPATH $HOME/workspace/gospace
 
 # etcd
 set -x ETCDCTL_API 3
 
-# python
-# alias python="python3"
-# alias pip="pip3"
-# export WORKON_HOME=$HOME/.virtualenvs
-# export PROJECT_HOME=$HOME/python-place
-# export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-# source ~/.local/bin/virtualenvwrapper.sh
-
 # autojump
-if test -f /home/bobo/.autojump/share/autojump/autojump.fish; . /home/bobo/.autojump/share/autojump/autojump.fish; end
+if test -f $HOME/.autojump/share/autojump/autojump.fish; . $HOME/.autojump/share/autojump/autojump.fish; end
+
+# PATH
+set -x PATH $PATH $HOME/.local/bin /usr/local/go/bin $GOPATH/bin $HOME/.cargo/bin
+
+# python virtual fish
+eval (python -m virtualfish)
+
+# editor
+set -x EDITOR vim
+
+# locale
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
 
 # hide greeting
 function fish_greeting
 end
+
+# powerline
+# https://powerline.readthedocs.io/en/latest/usage/shell-prompts.html#fish-prompt
+# install: `pip install powerline-status`
+# find root path: `pip show powerline-status`
+set fish_function_path $fish_function_path "$HOME/.local/lib/python3.8/site-packages/powerline/bindings/fish"
+powerline-setup
